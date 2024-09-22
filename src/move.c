d00459516890c3f815d6b89fb6e5684e9475b9be
Dalhatmd <umarwalid4055@gmail.com>
2024-09-22 19:56:32 +0100

    Fix bug where the player gets stuck on walls while strafing

--- src/move.c
+++ src/move.c
@@ -67,56 +67,43 @@ void handleInput(SDL_Event *event, RaycasterState *state)
  */
 void updatePosition(RaycasterState *state, double deltaTime)
 {
-	double moveSpeed, rotSpeed, speedMultiplier;
-	speedMultiplier = 0.005;
+    double moveSpeed, rotSpeed, speedMultiplier;
+    speedMultiplier = 0.005;
+    moveSpeed = MOVE_SPEED * deltaTime * speedMultiplier;
+    rotSpeed = ROTATE_SPEED * deltaTime * speedMultiplier;
 
-	moveSpeed = MOVE_SPEED * deltaTime * speedMultiplier;
-	rotSpeed = ROTATE_SPEED *+ deltaTime * speedMultiplier;
+    // Forward and backward movement
+    if(state->moveForward || state->moveBackward)
+    {
+        double moveDir = state->moveForward ? 1 : -1;
+        if(state->map[(int)(state->posX + state->dirX * moveSpeed * moveDir)][(int)state->posY] == 0)
+            state->posX += state->dirX * moveSpeed * moveDir;
+        if(state->map[(int)state->posX][(int)(state->posY + state->dirY * moveSpeed * moveDir)] == 0)
+            state->posY += state->dirY * moveSpeed * moveDir;
+    }
 
-	if(state->moveForward)
-	{
-		if(state->map[(int)(state->posX + state->dirX * moveSpeed)][(int)state->posY] == 0)
-			state->posX += state->dirX * moveSpeed;
-		if(state->map[(int)state->posX][(int)(state->posY + state->dirY * moveSpeed)] == 0)
-			state->posY += state->dirY * moveSpeed;
-	}
-	if(state->moveBackward)
-	{
-		if(state->map[(int)(state->posX - state->dirX * moveSpeed)][(int)state->posY] == 0)
-			state->posX -= state->dirX * moveSpeed;
-		if(state->map[(int)state->posX][(int)(state->posY - state->dirY * moveSpeed)] == 0)
-			state->posY -= state->dirY * moveSpeed;
-	}
-	if(state->rotateLeft)
-	{
-		double oldDirX = state->dirX;
-		state->dirX = state->dirX * cos(rotSpeed) - state->dirY * sin(rotSpeed);
-		state->dirY = oldDirX * sin(rotSpeed) + state->dirY * cos(rotSpeed);
-		double oldPlaneX = state->planeX;
-		state->planeX = state->planeX * cos(rotSpeed) - state->planeY * sin(rotSpeed);
-		state->planeY = oldPlaneX * sin(rotSpeed) + state->planeY * cos(rotSpeed);
-	}
-	if(state->rotateRight)
-	{
-		double oldDirX = state->dirX;
-		state->dirX = state->dirX * cos(-rotSpeed) - state->dirY * sin(-rotSpeed);
-		state->dirY = oldDirX * sin(-rotSpeed) + state->dirY * cos(-rotSpeed);
-		double oldPlaneX = state->planeX;
-		state->planeX = state->planeX * cos(-rotSpeed) - state->planeY * sin(-rotSpeed);
-		state->planeY = oldPlaneX * sin(-rotSpeed) + state->planeY * cos(-rotSpeed);
-	}
-	if (state->strafeLeft)
-	{
-		if (state->map[(int)(state->posX - state->dirX * moveSpeed)][(int)state->posY] == 0)
-			state->posX -= state->dirY * moveSpeed;
-		if (state->map[(int)state->posX][(int)(state->posY + state->dirX * moveSpeed)] == 0)
-			state->posY += state->dirX * moveSpeed;
-	}
-	if (state->strafeRight)
-	{
-		if (state->map[(int)(state->posX + state->dirX * moveSpeed)][(int)state->posY] == 0)
-			state->posX += state->dirY * moveSpeed;
-		if (state->map[(int)state->posX][(int)(state->posY - state->dirX * moveSpeed)] == 0)
-			state->posY -= state->dirX * moveSpeed;
-	}
+    // Strafing movement
+    if(state->strafeLeft || state->strafeRight)
+    {
+        double strafeDir = state->strafeRight ? 1 : -1;
+        double strafeDirX = state->dirY * strafeDir;
+        double strafeDirY = -state->dirX * strafeDir;
+        
+        if(state->map[(int)(state->posX + strafeDirX * moveSpeed)][(int)state->posY] == 0)
+            state->posX += strafeDirX * moveSpeed;
+        if(state->map[(int)state->posX][(int)(state->posY + strafeDirY * moveSpeed)] == 0)
+            state->posY += strafeDirY * moveSpeed;
+    }
+
+    // Rotation
+    if(state->rotateLeft || state->rotateRight)
+    {
+        double rotDir = state->rotateLeft ? 1 : -1;
+        double oldDirX = state->dirX;
+        state->dirX = state->dirX * cos(rotSpeed * rotDir) - state->dirY * sin(rotSpeed * rotDir);
+        state->dirY = oldDirX * sin(rotSpeed * rotDir) + state->dirY * cos(rotSpeed * rotDir);
+        double oldPlaneX = state->planeX;
+        state->planeX = state->planeX * cos(rotSpeed * rotDir) - state->planeY * sin(rotSpeed * rotDir);
+        state->planeY = oldPlaneX * sin(rotSpeed * rotDir) + state->planeY * cos(rotSpeed * rotDir);
+    }
 }
